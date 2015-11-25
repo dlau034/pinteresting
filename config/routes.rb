6ff586bb10f1e2fc90bec32988d4ff6aedb117f7
david lau <bbc093@hotmail.com>
2015-11-26 11:12:02 +1300

    Add functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :pins
   devise_for :users
-  root 'pages#home'
+  root 'pins#index'
+  # root 'pages#home'
 
   get 'about' => 'pages#about' # creates about_path
 
